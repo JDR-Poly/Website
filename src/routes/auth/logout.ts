7320ef6f1217d330cce9271a6e2685fbe28ce005
Eden Kahane <kahaneeden@protonmail.com>
2022-07-07 23:50:51 +0200

    Logout :door:

---
+++ src/routes/auth/logout.ts
@@ -0,0 +1,23 @@
+import type {RequestEvent} from "@sveltejs/kit";
+import { serialize } from "cookie";
+
+export async function post() {
+    const headers = {
+        "Set-Cookie": serialize(
+            "session_id", 
+            '', 
+            {
+                expires: new Date(Date.now() - 3600),
+                path: "/",
+                httpOnly: true,
+                secure: true,
+                sameSite: "strict"
+            }
+            )
+    }
+    
+    return {
+        status: 200,
+        headers
+    }
+}
