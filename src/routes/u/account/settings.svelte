223ac7569ca2f127d87d87e41b8e1b8881d1b95f
Eden Kahane <kahaneeden@protonmail.com>
2022-07-07 23:45:52 +0200

    Profile configuration :busts_in_silhouette:

---
+++ src/routes/u/account/settings.svelte
@@ -0,0 +1,19 @@
+<script context="module" lang="ts">
+    import type { Load } from '@sveltejs/kit'
+
+    export const load: Load = async (event) => {
+        if(!event.session.authenticated) {
+            return {
+                status: 302,
+                redirect: "/login"
+            }
+        } else {
+            return {
+                status: 200
+            }
+        }
+
+    }
+</script>
+
+<h2>Account setting</h2>
---
+++ src/routes/u/profile/[id].svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { goto } from "$app/navigation";
+import { page } from "$app/stores";
+
+const {id} = $page.params
+</script>
+
+
+<h2>{id}</h2>
+
+<button on:click={() => {
+    goto("/")
+}}>Acceuil</button>
