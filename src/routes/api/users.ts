3ba631d764733ba9ee963bc1120b9419d8dcd5b5
Eden Kahane <kahaneeden@protonmail.com>
2022-07-17 14:10:20 +0200

    Improved users request code :recycle:

--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -10,29 +10,19 @@ import type { User } from "src/types";
  * @param {number} request.number how many users to look for (max 100)
  * @param {number} request.index index from which to start returning users from (max 100)
  * @param {string} request.searchText name to look for
- * @return {User?} users found
+ * @return {User[]} users found
  */
 export async function post({request}: RequestEvent) {
     const body = await request.json()
-
-    const number = () => {
-        return Math.min(100, body.number)
-    }
+    const number = Math.min(100, body.number)
     const index = Math.min(100, body.index || 0)
-    const searchText = () => {
-        if(body.searchText) {
-            return "%" + body.searchText + "%"
-        } else {
-            return "%"
-        }
-    }
-
-    const res = await db.any(USER_SEARCH, {
+    const searchText = (body.searchText != undefined ? "%" + body.searchText + "%" : "%")
+    let res = await db.any(USER_SEARCH, {
         table: "users",
         searchText: searchText,
         number: number,
         index: index
-    })
+    }) || []
     return {
         status: 200,
         headers: {
