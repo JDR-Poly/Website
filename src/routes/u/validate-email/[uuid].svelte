0ec08967d7c4ca1062e823dee1a693cd52636576
Eden Kahane <kahaneeden@protonmail.com>
2022-07-20 19:07:58 +0200

    Working mail validation links :email:

---
+++ src/routes/u/validate-email/[uuid].svelte
@@ -0,0 +1,67 @@
+<script context="module" lang="ts">
+    import type { Load } from '@sveltejs/kit'
+
+    export const load: Load = async (event) => {
+        if(!event.session.authenticated) {
+            return {
+                status: 302,
+                redirect: "/u/login"
+            }
+        } else if(event.session.user?.isEmailValidated !== false) {
+            return {
+                status: 302,
+                redirect: "/"
+            }
+        } else {
+            return {
+                status: 200
+            }
+        }
+    }
+</script>
+
+<script lang="ts">
+    import { page } from "$app/stores";
+    import { user } from '../../stores';
+    import { goto } from '$app/navigation';
+
+    const {uuid} = $page.params
+
+    const emailValidationResult = fetch("/auth/validate-email", {
+                method: "POST",
+                body: JSON.stringify({uuid}),
+                headers: {"Content-Type" : "application/json"}
+            }).then(res => {
+                if(res.ok) {
+                    setTimeout(() => location.reload(), 2000)
+                }
+                return res
+            })
+</script>
+
+{#await emailValidationResult}
+    <h1>Loading</h1>
+{:then res} 
+    {#if res.ok}
+        <p>Votre mail est validé ! Nous allons vous rediriger vers la page d'accueil</p>
+    {:else}
+        <p>Ce lien n'est plus valide</p>
+        <button on:click={async () => {
+            try {
+                const res = await fetch("/auth/send-email-validation", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        id: $user.id
+                    }),
+                    headers: {"Content-Type" : "application/json"}
+                })
+                if(res.ok) {
+                    console.log("mail envoyé")
+                    goto("/u/validate-email")
+                }
+            } catch(err) {
+                console.error(err)
+            }
+        }}>Envoyer un nouveau mail</button>
+    {/if}
+{/await}
---
+++ src/routes/u/validate-email/index.svelte
@@ -0,0 +1,52 @@
+<script context="module" lang="ts">
+    import type { Load } from '@sveltejs/kit'
+
+    export const load: Load = async (event) => {
+        if(!event.session.authenticated) {
+            return {
+                status: 302,
+                redirect: "/u/login"
+            }
+        } else if(event.session.user?.isEmailValidated !== false) {
+            return {
+                status: 302,
+                redirect: "/"
+            }
+        } else {
+            return {
+                status: 200
+            }
+        }
+    }
+</script>
+
+<script lang="ts">
+    import { authenticated, user } from "../../stores";
+</script>
+
+<h>En attente de validation du mail</h>
+
+<p>Vous avez dû recevoir un mail de validation à l'email: {$user.email}</p>
+
+<button on:click={async () => {
+    try {
+        const res = await fetch("/auth/send-email-validation", {
+            method: "POST",
+            body: JSON.stringify({
+                id: $user.id
+            }),
+            headers: {"Content-Type" : "application/json"}
+        })
+        if(res.ok) {
+            console.log("mail envoyé")        }
+    } catch(err) {
+        console.error(err)
+    }
+}}>Renvoyez un mail</button>
+
+<button on:click={ async () => {
+    const res = await fetch("/auth/logout", {method: "POST"})
+    authenticated.set(false)
+    user.set({id: 0})
+    location.reload()
+}}>Mauvais email ?</button>
