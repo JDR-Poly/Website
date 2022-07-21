c57801b6a19efd042657c8c470453e956ad0360d
Eden Kahane <kahaneeden@protonmail.com>
2022-07-21 22:01:43 +0200

    Reorganized ModifyRole component :wrench:

--- src/lib/components/admin/user/ModifyRole.svelte
+++ src/lib/components/admin/user/ModifyRole.svelte
@@ -3,7 +3,6 @@
 
 	export let user: User;
 
-	let error = '';
 	let roleName = user.role?.name;
 
 	const rolesQuery = fetch('/api/admin/roles/modifiableRoles', {
@@ -11,26 +10,24 @@
 		body: JSON.stringify({ id: user.id }),
 		headers: { 'Content-Type': 'application/json' }
 	}).then((res) => (res.ok ? res.json().then((json) => json.roles) : []));
-</script>
 
-{#await rolesQuery}
-	<p>Loading role</p>
-{:then roles}
-	<!-- Role change -->
-	<form
-		on:submit|preventDefault={async () => {
-			const res = await fetch('/api/admin/roles/changeRole', {
+	async function submitRoleChange() {
+		const res = await fetch('/api/admin/roles/changeRole', {
 				method: 'POST',
 				body: JSON.stringify({ user, role: roleName }),
 				headers: { 'Content-Type': 'application/json' }
 			});
 			if (res.ok) {
 				location.reload();
-			} else {
-				error = "Vous n'êtes pas autorisé à changé ce rôle";
 			}
-		}}
-	>
+	}
+</script>
+
+{#await rolesQuery}
+	<p>Loading role</p>
+{:then roles}
+	<!-- Role change -->
+	<form on:submit|preventDefault={submitRoleChange}>
 		<select bind:value={roleName} disabled={roles.length === 0}>
 			{#if roles.length === 0}
 				<option value={roleName}>{roleName}</option>
