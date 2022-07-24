28c382a3fa1d16501879d0d5252b3e0772159305
Eden Kahane <kahaneeden@protonmail.com>
2022-07-24 20:56:54 +0200

    Added mail sent function :wrench:

--- src/lib/backend/mailClient.ts
+++ src/lib/backend/mailClient.ts
@@ -2,9 +2,28 @@ import { createTestAccount, createTransport, type Transporter, getTestMessageUrl
 import { v4 as uuid } from "uuid"
 import { db } from "./postgresClient";
 
-
-let transporter: Transporter
-if (import.meta.env.DEV) {
+let transporter: Transporter | undefined
+let ethereal = false
+if (import.meta.env.PROD) {
+	const testTransporter = createTransport({
+		host: process.env.MAIL_HOST,
+		port: parseInt(process.env.MAIL_PORT || "465"),
+		secure: (process.env.MAIL_PORT === "465" || process.env.MAIL_PORT === undefined),
+		auth: {
+			user: process.env.MAIL_USER,
+			pass: process.env.MAIL_PASSWORD
+		}
+	});
+	testTransporter.verify((error, success) => {
+		if (!error) {
+			transporter = testTransporter
+			console.log("Email transporter is correctly linked")
+		} else {
+			console.log(error)
+		}
+	});
+}
+if (transporter == undefined) {
 	let testAccount = await createTestAccount(); //Generate email using ethereal
 	transporter = createTransport({
 		host: "smtp.ethereal.email",
@@ -15,16 +34,24 @@ if (import.meta.env.DEV) {
 			pass: testAccount.pass // generated ethereal password
 		}
 	});
-} else {
-	transporter = createTransport({
-		host: process.env.MAIL_HOST,
-		port: parseInt(process.env.MAIL_PORT || "465"),
-		secure: (process.env.MAIL_PORT === "465" || process.env.MAIL_PORT === undefined),
-		auth: {
-			user: process.env.MAIL_USER,
-			pass: process.env.MAIL_PASSWORD
-		}
-	});
+	ethereal = true
+}
+
+async function sendEmail(to: any, subject: string, html: string): Promise<any> {
+	try {
+		const result = await transporter!.sendMail({
+			from: '"JDRPoly Info" <informatique@jdrpoly.ch>',
+			to: to,
+			subject: subject,
+			html: html
+		})
+		if (ethereal) console.log("Preview Mail: %s", getTestMessageUrl(result));
+		return result
+	} catch (err) {
+		console.log(err)
+		return err
+	}
+
 }
 
 async function sendEmailValidationToken(userId: number, mail: string, origin: string) {
@@ -35,22 +62,10 @@ async function sendEmailValidationToken(userId: number, mail: string, origin: st
 		id: userId,
 		validation_token: emailValidationToken
 	})
-	try {
-		const result = await transporter.sendMail({
-			from: '"JDRPoly Info" <informatique@jdrpoly.ch>',
-			to: mail,
-			subject: "JDRPoly: Validez votre mail", // Subject line
-			html: '<a href="' + origin + '/u/validate-email/' + emailValidationToken + '" target="_blank">Hello world?</a>'
-		})
-		if (import.meta.env.DEV) console.log("Preview Mail: %s", getTestMessageUrl(result));
-	} catch (err) {
-		console.error(err)
-	}
-
-
+	sendEmail(mail, "JDRPoly: Validez votre mail", '<a href="' + origin + '/u/validate-email/' + emailValidationToken + '" target="_blank">Valider</a>')
 }
 
-export { sendEmailValidationToken, transporter }
+export { sendEmailValidationToken, sendEmail }
 
 const DB_EMAIL_VALIDATION_TOKEN = "INSERT INTO $[table:name](id, validation_token) VALUES($[id],$[validation_token])" +
 	"ON CONFLICT (id) DO UPDATE SET validation_token=$[validation_token]"
