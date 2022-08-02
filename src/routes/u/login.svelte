fbd8651c00221a3f1db398ba41901aeecce44d8a
Eden Kahane <kahaneeden@protonmail.com>
2022-08-02 17:13:14 +0200

    Additional formating :pencil2:

--- src/routes/u/login.svelte
+++ src/routes/u/login.svelte
@@ -1,66 +1,63 @@
 <script context="module" lang="ts">
-    import { redirectIfAuthenticated } from '$lib/frontend/redirect';
-    import type { Load } from '@sveltejs/kit'
+	import { redirectIfAuthenticated } from '$lib/frontend/redirect';
+	import type { Load } from '@sveltejs/kit';
 
-    export const load: Load = async (event) => {
-        return redirectIfAuthenticated(event, "/u/profile/" + event.session.user?.id)
-    }
+	export const load: Load = async (event) => {
+		return redirectIfAuthenticated(event, '/u/profile/' + event.session.user?.id);
+	};
 </script>
 
-
 <script lang="ts">
-	import {error} from  "$lib/stores"
-    let email = "";
-    let password = "";
+	import { error } from '$lib/stores';
+	let email = '';
+	let password = '';
 
-    async function login() {
-        try {
-
-            const res = await fetch("/auth/login", {
-                method: "POST",
-                body: JSON.stringify({
-                    email,
-                    password
-                }),
-                headers: {
-                    "Content-Type" : "application/json"
-                }
-            })
-            const body = await res.json()
-            if(res.ok) {
-                location.reload()
-            } else {
-                $error = body.message
-            }
-        } catch (err) {
-            console.log(err)
-            $error = "An error occured"
-        }
-    }
+	async function login() {
+		try {
+			const res = await fetch('/auth/login', {
+				method: 'POST',
+				body: JSON.stringify({
+					email,
+					password
+				}),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+			const body = await res.json();
+			if (res.ok) {
+				location.reload();
+			} else {
+				$error = body.message;
+			}
+		} catch (err) {
+			console.log(err);
+			$error = 'An error occured';
+		}
+	}
 </script>
 
 <div>
-    <h1>Se connecter</h1>
+	<h1>Se connecter</h1>
 
-    <input type="email" bind:value={email} placeholder="Mail">
-    <input type="password" bind:value={password} placeholder="Mot de passe">
+	<input type="email" bind:value={email} placeholder="Mail" />
+	<input type="password" bind:value={password} placeholder="Mot de passe" />
 
-    <button on:click={login}>Se connecter</button>
-
+	<button on:click={login}>Se connecter</button>
 </div>
 
 <a href="/u/register">Vous ne possédez pas de compte ?</a>
 
 <style>
-    div {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-    }
-    input {
-        height: 2rem;
-        width: 30%;
-        margin: 12px;
-    }
+	div {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-evenly;
+		align-items: center;
+	}
+	input {
+		height: 2rem;
+		width: 30%;
+		margin: 12px;
+	}
 </style>
