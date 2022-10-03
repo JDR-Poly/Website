7ce06c0c4524ceaea40fcb60b2c3ed746027cd66
Eden Kahane <kahaneeden@protonmail.com>
2022-10-03 02:57:17 +0200

    Page option to delete the event :sparkles:

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-	import { error } from "$lib/stores";
+	import { error, warning } from "$lib/stores";
 	import { page } from '$app/stores';
 	import { hasRolePermission, UserPermission } from "$lib/userPermissions";
+	import { goto } from "$app/navigation";
 
 	const eventsPromise = fetch('/api/events')
 		.then((res) => {
@@ -11,6 +12,18 @@
 			$error = err.message
 			return []
 		})
+
+	async function deleteEvent(id: number) {
+		const res = await fetch('/api/events/' + id, {
+			method: 'DELETE'
+		});
+		if (res.ok) {
+			location.reload()			
+		} else {
+			const body = await res.json();
+			$error = body.message
+		}
+	}
 </script>
 
 {#if hasRolePermission(UserPermission.CREATE_EVENT, $page.data.user?.role)}
@@ -24,6 +37,9 @@
 			<h5><a href={"/events/" + event.id} >{event.title}</a></h5>
 			<p>Date: {event.date}</p>
 			<p>{event.description}</p>
+			{#if hasRolePermission(UserPermission.MODIFY_EVENT, $page.data.user?.role)}
+				<button on:click={() => deleteEvent(event.id)}>Supprimer</button>
+			{/if}
 		</div>
 	{/each}
 {/await}
--- src/routes/events/[id]/+page.svelte
+++ src/routes/events/[id]/+page.svelte
@@ -2,7 +2,8 @@
 	import { error } from '$lib/stores';
 	import { page } from '$app/stores';
 	import { redirect } from '@sveltejs/kit';
-	import { hasRolePermission, Role, Roles } from '$lib/userPermissions';
+	import { hasRolePermission, Role, Roles, UserPermission } from '$lib/userPermissions';
+	import { goto } from '$app/navigation';
 
 	const { id } = $page.params;
 
@@ -35,6 +36,18 @@
 				break;
 		}
 	}
+
+	async function deleteEvent(id: number) {
+		const res = await fetch('/api/events/' + id, {
+			method: 'DELETE'
+		});
+		if (res.ok) {
+			goto("/events")	
+		} else {
+			const body = await res.json();
+			$error = body.message
+		}
+	}
 </script>
 
 {#await eventPromise}
@@ -55,5 +68,9 @@
 		{/if}
 	{:else}
 		<p color="green">Il n'y a pas besoin de s'inscrire pour cette événements</p>
+	{/if}
+
+	{#if hasRolePermission(UserPermission.MODIFY_EVENT) || event.author === $page.data.user?.id}
+		<button color="red" on:click={() => deleteEvent(event.id)}>Supprimer</button>
 	{/if}
 {/await}
