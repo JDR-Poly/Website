c092ffd07d8ee2067e5188976a0104a177aecd8e
Eden Kahane <kahaneeden@protonmail.com>
2023-03-10 20:46:38 +0100

    Added generic component

---
+++ src/lib/components/ImgDefault.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+
+	export let url: string;
+	export let defaultUrl: string;
+	export let alt: string | undefined;
+
+	let tryImageUrl = ""
+	
+	onMount(async () => {
+		tryImageUrl = await fetch(url)
+			.then((res) => {
+				if(res.ok) return url
+				else return defaultUrl
+			})
+			.catch((_) => {
+				return defaultUrl
+			})
+	}) 
+</script>
+
+<img src={tryImageUrl} {alt}/>
