3581344bdabded3fbd91ded8ec0cf4ced44c9eb9
Eden Kahane <kahaneeden@protonmail.com>
2023-03-12 19:32:44 +0100

    Added UI for profile

--- src/routes/users/profile/[id]/+page.svelte
+++ src/routes/users/profile/[id]/+page.svelte
@@ -5,17 +5,64 @@
 	const { id } = $page.params;
 
 	export let data: any;
+
+	const dateFormater = new Intl.DateTimeFormat('fr-Fr', {
+		dateStyle: 'long',
+		timeZone: 'Europe/Paris'
+	})
 </script>
 
 <svelte:head>
-	<title>Profile | JDRPoly</title> 
+	<title>{data.user_profile.name} | JDRPoly</title> 
 </svelte:head>
 
-<h1>Profile</h1>
-<p>Email: {data.user_profile.email}</p>
-<p>Nom: {data.user_profile.name}</p>
-<p>Role: {data.user_profile.role?.name}</p>
-<p>Date de création: {data.user_profile.account_creation}</p>
-{#if $page.data.authenticated && hasRolePermission(UserPermission.MODIFY_USERS_DATA, $page.data.user.role)}
-		<a href="/admin/profile/{id}">Modifier les données de cet utilisateur</a>
-{/if}
+<main>
+	<h2>Profile</h2>
+
+	{#if hasRolePermission(UserPermission.SEE_MAIL, $page.data.user.role)}
+		<p>Email: <strong>{data.user_profile.email}</strong></p>
+	{/if}
+	<p>Nom: <strong>{data.user_profile.name}</strong></p>
+	<p>Date de création: <strong>{dateFormater.format(Date.parse(data.user_profile.account_creation))}</strong></p>
+	<p>Role: <strong>{data.user_profile.role?.name}</strong></p>
+
+	{#if data.user_profile.member_start}
+		<p>Membre depuis: <strong>{dateFormater.format(Date.parse(data.user_profile.member_start))}</strong></p>
+	{/if}
+	{#if data.user_profile.member_stop}
+		<p>Fin de membre: <strong>{dateFormater.format(Date.parse(data.user_profile.member_stop))}</strong></p>
+	{/if}
+
+	{#if hasRolePermission(UserPermission.MODIFY_USERS_DATA, $page.data.user.role)}
+			<a href="/admin/profile/{id}">Modifier les données de cet utilisateur</a>
+	{/if}
+</main>
+
+<style lang="scss">
+	main {
+		width: 70%;
+		margin: 8em auto;
+		min-height: 40vh;
+		color: #777;
+		font-family: 'Ubuntu';
+
+		p,a {
+			font-size: 20px;
+			margin: 4px;
+		}
+		strong {
+			color: #4b4b4b;;
+		}
+		a {
+			margin-top: 1em;
+			display: block;
+		}
+
+		h2 {
+			text-transform: uppercase;
+			font-weight: 600;
+			letter-spacing: 0.15em;
+			margin-bottom: 15px;
+		}
+	}
+</style>
