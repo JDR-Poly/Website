05b02f80f86f05ebf61604d48bfef693bb3e5f4f
Eden <kahaneeden@protonmail.com>
2023-05-09 00:36:10 +0200

    Added expiration deletion/update

--- src/lib/server/postgresClient.ts
+++ src/lib/server/postgresClient.ts
@@ -1,5 +1,6 @@
 import pgPromise from 'pg-promise';
 import { env } from '$env/dynamic/private';
+import { schedule } from 'node-cron'
 
 const pgp = pgPromise()
 
@@ -10,6 +11,12 @@ const db = pgp({
 	user:  env.DB_USER,
 	password: env.DB_PASSWORD,
 	max: 50
+});
+
+schedule('0 1 * * *', () => {
+	db.none(`UPDATE users SET role='USER', member_start=NULL, member_stop=NULL WHERE NOW() >= member_stop AND role='MEMBER'`)
+	db.none(`DELETE FROM email_validation e USING users u WHERE e.id = u.id AND u.is_email_validated`)
+	db.none(`DELETE FROM sessions WHERE NOW() > expiration_date`)
 });
 
 export { db }
