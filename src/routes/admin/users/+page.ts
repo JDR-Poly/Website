a6e2e7e243ed351c9cdd38f1c50e22b194684823
Eden Kahane <kahaneeden@protonmail.com>
2023-11-15 21:36:12 +0100

    Removed useless reverse

--- src/routes/admin/users/+page.ts
+++ src/routes/admin/users/+page.ts
@@ -8,10 +8,10 @@ export const load = (async ({ fetch }) => {
 				return res.json()
 			}).then((events: User[]) => {
 				return events.sort((a, b) => {
-					if(a.id > b.id) return -1 
+					if(a.id > b.id) return 1 
 					else if(a.id == b.id) return 0
-					else return 1
-				}).reverse()
+					else return -1
+				})
 			})
 	};
 }) satisfies PageLoad;
