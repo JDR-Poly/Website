50e9bb73a7e3a3918c78f42548a1547eee9070e1
Eden Kahane <kahaneeden@protonmail.com>
2023-11-17 19:09:53 +0100

    Made default request for user search infinite length

--- src/routes/api/users/search/+server.ts
+++ src/routes/api/users/search/+server.ts
@@ -6,7 +6,7 @@ import type { RequestHandler } from "./$types";
 /**
  * Search a certain number of userwhere user/mail matches
  * text (with an index) by id decreasing
- * @param {number} url.searchParams.number how many users to look for
+ * @param {number} url.searchParams.number how many users to look for (default all)
  * @param {number} url.searchParams.index index from which to start returning users from
  * @param {string} url.searchParams.searchText name to look for
  * @return {User[]} users found
@@ -16,7 +16,7 @@ export const GET = (async ({ url, locals }) => {
 	if(!locals.authenticated) throw error(401)
  	if(!hasRolePermission(UserPermission.SEE_USERS_PROFILE, locals.user?.role)) throw error(403)
 
-	const number = parseInt(url.searchParams.get("number") || "1") || 1
+	const number = parseInt(url.searchParams.get("number") || "null") || null
 	const index = parseInt(url.searchParams.get("index") || "0") || 0
 	const searchText = (url.searchParams.get("searchText") != undefined ? "%" + (url.searchParams.get("searchText") || "") + "%" : "%")
 	const mailSQLText = locals.authenticated && hasRolePermission(UserPermission.SEE_MAIL, locals.user?.role) ? "email, " : "" 
