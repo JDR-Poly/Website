9d50566eb9a5902c133e3e8aad5f29a228fd863c
EdenKahane <kahaneeden@protonmail.com>
2024-05-18 02:27:07 +0200

    Fix node-pre-gyp (added by bcrypt) wrong dependency declaration.

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ import { sveltekit } from "@sveltejs/kit/vite";
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
+	optimizeDeps: {
+		/**
+		 * Fix node-pre-gyp (added by bcrypt) wrong dependency declaration.
+		 * @see https://stackoverflow.com/questions/77721584/react-vite-app-shows-the-error-no-loader-is-configured-for-html-files-se
+		 */
+		exclude: ["@mapbox"], 
+	  },
 };
 
 export default config;
