a22513993c9a7d0f75f0e7f988c0f284475689ca
EdenKahane <kahaneeden@protonmail.com>
2024-09-04 04:06:36 +0200

    Added 2024-2025 categorie

--- src/routes/api/committee/categories/+server.ts
+++ src/routes/api/committee/categories/+server.ts
@@ -3,7 +3,7 @@
 import { json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
-const CATEGORIES = ["2022-2023", "2023-2024"].reverse(); //order should be ["2021-2022", "2022-2023"]
+const CATEGORIES = ["2022-2023", "2023-2024", "2024-2025"].reverse(); //order should be ["2021-2022", "2022-2023"]
 
 /**
  * Get all the years of the committee
